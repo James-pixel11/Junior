c9034a809715f0ea2318a8359cbe39ff90d11619
James-pixel11 <56347994+James-pixel11@users.noreply.github.com>
2019-10-09 14:36:02 +0200

    Added CalculatorShop Project Source Code

---
+++ CalculatorShop/CalculatorShop/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorShop
+{
+    private OleDbConnection connection = new OleDbConnection();
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();     
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          try
+
+            {
+               
+              
+connection.Connection.String = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source = C:\Users\Mukazika\Documents\mydatabase.accdb";
+               
+             connection.Open();
+             checkConnection.Text = "SUCCESSFUL!";
+                
+             connection.Close();
+               }   
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error" + ex);
+            }
+        }
+    }
+}
+
+
+              
+
+                
+         
